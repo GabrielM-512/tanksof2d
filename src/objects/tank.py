9e0415313741960385df678ec5fc6877148de614
Furkan <kocerkan1980@gmail.com>
2025-09-22 23:55:01 +0200

    sigma calc

--- src/objects/tank.py
+++ src/objects/tank.py
@@ -2,19 +2,24 @@ import pygame
 import math
 from src.objects.Hitbox import Hitbox
 from src.objects.Shooting import Bullet
+import numpy as np
+
 
 class Tank:
     def __init__(self):
-        self.icon_base = pygame.image.load("assets/PNG/Weapon_Color_D/Gun_02.png")
+        self.icon_base = pygame.image.load("assets/PNG/Hulls_Color_D/Hull_02.png")
         self.icon_display = self.icon_base
+        self.chasis_icon = pygame.image.load("assets/PNG/Weapon_Color_D/Gun_01.png")
+        self.chasis_display = self.icon_base
         self.rect = Hitbox(self.icon_display,10,10)
         self.bullets = []
         self.health = 100
         self.maxBullets = 5
         self.angle = 0
+        self.chasisPosX,self.chasisPosY = self.find_black_circle_center("assets/PNG/Hulls_Color_D/Hull_02.png")
 
     def move(self):
-        self.turn_turret(0)
+        self.turn_turret()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.rect.y -= 5
@@ -27,23 +32,14 @@ class Tank:
         if pygame.mouse.get_pressed()[0]:
             self.shoot(pygame.mouse.get_pos())
 
-    def turn_turret(self, angle):
-        try:
-            mouse_pos = pygame.mouse.get_pos()
-            newRect = self.icon_display.get_rect(center=(self.rect.x + self.icon_display.get_width() // 2, self.rect.y + self.icon_display.get_height() // 2))
-            rot_degree = math.atan((mouse_pos[0] - newRect.centerx) / (mouse_pos[1] - newRect.centery))
-            if mouse_pos[1] - newRect.centery > 0:
-                rot_degree += math.pi
-                
-            self.icon_display = pygame.transform.rotate(self.icon_base, rot_degree * 180 / math.pi)
-            self.angle = rot_degree
-        except ZeroDivisionError:
-            if mouse_pos[0] - newRect.centerx < 0:
-                self.icon_display = pygame.transform.rotate(self.icon_base, 90)
-                self.angle = 90
-            elif mouse_pos[0] - newRect.centerx > 0:
-                self.icon_display = pygame.transform.rotate(self.icon_base, -90)
-                self.angle = -90
+    def turn_turret(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        tank_center_x = self.rect.x + self.chasisPosX
+        tank_center_y = self.rect.y + self.chasisPosY
+        delta_x = mouse_x - tank_center_x
+        delta_y = mouse_y - tank_center_y
+        self.angle = math.degrees(math.atan2(-delta_y, delta_x)) - 90
+        self.chasis_display = pygame.transform.rotate(self.chasis_icon, self.angle)
 
     def shoot(self,pos):
         bullet = Bullet(self.bullets,(self.rect.x,self.rect.y),pos,self.angle)
@@ -54,10 +50,34 @@ class Tank:
         #olist = self.rect.hitbox.outline()
         #pygame.draw.polygon(screen,(200,150,150),olist,0)
         screen.blit(self.icon_display, (self.rect.x, self.rect.y))
+        pygame.draw.circle(screen, (255, 0, 0), (self.rect.x + self.chasisPosX, self.rect.y + self.chasisPosY), 5)
+        print(self.find_black_circle_center("assets/PNG/Hulls_Color_D/Hull_02.png"))
+
+        screen.blit(
+            self.chasis_display,
+            (
+                self.chasisPosX - self.chasis_display.get_width() // 2 + self.rect.x,
+                self.chasisPosY - self.chasis_display.get_height() // 2 + self.rect.y -50
+            )
+        )
 
     def update(self, screen):
+        self.draw(screen)
         self.rect.update(self.icon_display)
         if len(self.bullets) > 0:
             for bullet in self.bullets:
                 bullet.update(screen)
-        self.draw(screen)
+
+    def find_black_circle_center(self, image_path):
+        surf = pygame.image.load(image_path).convert_alpha()
+        arr = pygame.surfarray.array3d(surf)
+
+        mask = np.all(arr < [20, 20, 20], axis=2) 
+        ys, xs = np.where(mask)
+
+        if len(xs) == 0 or len(ys) == 0:
+            raise ValueError("No black dot found!")
+
+        cx = int(xs.mean())
+        cy = int(ys.mean()) + 23
+        return cx, cy

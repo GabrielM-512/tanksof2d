0e0b11f0a3e3dcf42cb42d9ac8d9664d9d8ec615
GabrielM-512 <gabriel.maurer@gmx.de>
2025-09-22 11:15:58 +0200

    tank turning

--- src/objects/tank.py
+++ src/objects/tank.py
@@ -1,7 +1,7 @@
 import pygame
 import math
 
-class tank:
+class Tank:
 
     def __init__(self):
         self.rect = pygame.rect.Rect()
@@ -18,15 +18,16 @@ class tank:
             if mouse_pos[1] - self.rect.centery > 0:
                 rot_degree += math.pi
                 
-            square_gfx = pygame.transform.rotate(square_gfx_template, rot_degree * 180 / math.pi)
+            square_gfx = pygame.transform.rotate(self.icon_base, rot_degree * 180 / math.pi)
+
         except ZeroDivisionError:
-            if mouse_pos[0] - square.centerx < 0:
-                square_gfx = pygame.transform.rotate(square_gfx_template, 90)
-            elif mouse_pos[0] - square.centerx > 0:
-                square_gfx = pygame.transform.rotate(square_gfx_template, -90)
+            if mouse_pos[0] - self.rect.centerx < 0:
+                square_gfx = pygame.transform.rotate(self.icon_base, 90)
+            elif mouse_pos[0] - self.rect.centerx > 0:
+                square_gfx = pygame.transform.rotate(self.icon_base, -90)
 
     def shoot(self):
         pass
     
-    def update(self):
-        self.move()
+    def draw(self, screen : pygame.display):
+        screen.blit(self.icon_display, self.rect)
